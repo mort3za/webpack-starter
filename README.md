def342766a0a34f76ec7348dcb40e8d6793d5a58
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-03-07 13:28:08 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Starter project  
+Uses webpack, webpack-cli, webpack-dev-server, babel.  
+
+## Commands  
+`npm start` to open in browser in development mode.  
+`npm dev` to run webpack once in development mode.  
+`npm build` to build.  
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Sample!',
+    template: 'src/index.html'
+  })]
+};
