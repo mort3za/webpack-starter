0b0a8e8325c59136e3a6fa6d141f417eabdf9656
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-03 14:06:28 +0430

    update packages

---
+++ README.md
@@ -0,0 +1,9 @@
+# Moved to http://github.com/mort3za/webpack-starter  
+
+# Starter project  
+Uses webpack, webpack-cli, webpack-dev-server, babel.  
+
+## Commands  
+`npm start` to open in browser in development mode.  
+`npm dev` to run webpack once in development mode.  
+`npm build` to build.  
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+};
